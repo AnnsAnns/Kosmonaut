c8f506a834236c593f1b559b6b79ecce4ff4fa8a
tumGER <git@tomger.eu>
2023-05-21 23:44:16 +0200

    Fix ts warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div
       className="absolute grid min-h-full min-w-full grid-cols-3 content-center bg-no-repeat"
